0a0a82125c29f1fa978f03a9c934ece0cdee0103
Sonia Huoy <vsoniahuoy@gmail.com>
2019-05-09 04:48:17 +0000

    Lesson 14 make the submit button work by redirect the item to the database

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,8 +5,19 @@ class PlacesController < ApplicationController
     @places = Place.order("name").page(params[:page]).per_page(5)  
   end
 
-  def new
+ def new
     @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 
 end
