0c334c1578b0f77f49fe2de1b58848a0dc13795d
Sonia Huoy <vsoniahuoy@gmail.com>
2019-05-12 01:44:29 +0000

    Allow only user logged in can create new places on the form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # configure the mailer so it knows how to build the URLs.
-  config.action_mailer.default_url_options = {host: 'localhost:3030'}
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
