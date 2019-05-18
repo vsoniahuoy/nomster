efb85dc275ecc121c4b04f92a398fd8c7838ff59
Sonia Huoy <vsoniahuoy@gmail.com>
2019-05-18 19:05:49 +0000

    change the route.rb to match the lee orignal lesson and still does not work

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   as :user do
+#   get '/users/sign_out' => 'devise/sessions#destroy'
+#   get '/place/:id' => 'places#destroy'
+#   end
+
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root 'places#index'
+#   resources :places
+# end
+
 Rails.application.routes.draw do
   devise_for :users
-  as :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-  get '/place/:id' => 'places#destroy'
-  end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
-  resources :places
+  resources :places do
+    resources :comments, only: :create
+  end
 end
