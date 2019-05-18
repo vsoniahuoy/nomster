c782a160c6cf238a0a1a31785c6155dfbd1f18d1
Sonia Huoy <vsoniahuoy@gmail.com>
2019-05-18 18:46:03 +0000

    Destory a place does not  work

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,7 +27,13 @@ class PlacesController < ApplicationController
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
-  redirect_to root_path
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   as :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
+  get '/place/:id' => 'places#destroy'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
