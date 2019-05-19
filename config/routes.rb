a0df61aaa075713b8b381c19473309255ce9dd58
Sonia Huoy <vsoniahuoy@gmail.com>
2019-05-19 18:18:40 +0000

    delete with prompt does not work

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   as :user do
-#   get '/users/sign_out' => 'devise/sessions#destroy'
-#   get '/place/:id' => 'places#destroy'
-#   end
-
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   root 'places#index'
-#   resources :places
-# end
-
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  get '/places/:id' => 'places#destroy'
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
-  resources :places do
-    resources :comments, only: :create
-  end
+  resources :places
 end
+
+# Rails.application.routes.draw do4
+#   devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root 'places#index'
+#   resources :places do
+#     resources :comments, only: :create
+#   end
+# end
