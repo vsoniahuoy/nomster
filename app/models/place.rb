a7d9e146745176b12887e7fc6c463eb41ab642e9
Sonia Huoy <vsoniahuoy@gmail.com>
2019-06-06 04:18:18 +0000

    Nomster, Lesson 32

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
 
-  validates :address, :description, presence: true
   validates :name, presence: true, length: { minimum: 3 } 
+  validates :address, :description, presence: true  
 end
