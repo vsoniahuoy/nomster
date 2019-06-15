4468b989db01d3e720efba6e8ee569ec416b2d34
Sonia Huoy <vsoniahuoy@gmail.com>
2019-06-15 23:52:06 +0000

    both model link to one

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,15 +3,15 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(comment_params.merge(user: current_user))
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
 
   private
 
-  def comment_params
-    params.require(:photo).permit(:picture, :caption)
+  def photo_params
+      params.require(:photo).permit(:picture, :caption)
   end
 
 end
