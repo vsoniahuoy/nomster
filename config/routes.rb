36dbbdbd80776abeb81b6889c7cb05a8ce630bc4
Sonia Huoy <vsoniahuoy@gmail.com>
2019-06-17 18:19:00 +0000

    setup profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
   resources :places do 
-    resources :comments, :photos, only: :create
+    resources :comments, only: :create
+    resources :photos, only: :create
   end
+  resources :users, only: :show
 end
 
